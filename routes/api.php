7e45b9b5e61193262fe4b53d3d19fb2d46ebb7ca
JOYCEWONG-2025 <25005859@siswa.um.edu.my>
2026-06-01 19:33:37 +0800

    Fix duplicate route names between web and api

--- routes/api.php
+++ routes/api.php
@@ -4,5 +4,11 @@ use App\Http\Controllers\Api\JobController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->group(function () {
-    Route::apiResource('jobs', JobController::class);
+    Route::apiResource('jobs', JobController::class)->names([
+        'index' => 'api.jobs.index',
+        'store' => 'api.jobs.store',
+        'show' => 'api.jobs.show',
+        'update' => 'api.jobs.update',
+        'destroy' => 'api.jobs.destroy',
+    ]);
 });
