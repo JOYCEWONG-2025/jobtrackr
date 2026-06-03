4ee9982d4185054c98e0d1114fe05f35d1b8afa5
JOYCEWONG-2025 <25005859@siswa.um.edu.my>
2026-06-03 16:52:17 +0800

    Redirect homepage to jobs

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use App\Http\Controllers\JobController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('jobs.index');
 });
 
 Route::get('/dashboard', function () {
